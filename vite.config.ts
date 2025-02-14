8a21d29ab090e439d81eec2f142ad67bd9f6b063
정택유 <wdjty326@gmail.com>
2025-02-14 22:49:51 +0900

    react-swc 로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactSWC from '@vitejs/plugin-react-swc'
 import {vanillaExtractPlugin} from '@vanilla-extract/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [reactSWC(), vanillaExtractPlugin()],
   build: {
     rollupOptions: {
       output: {
